ba37f88fad30b277422b791c7136a5b259061b11
harunamarun <harunamarun@gmail.com>
2020-01-14 13:59:16 +0900

    fixed sort()

--- src/Sort.js
+++ src/Sort.js
@@ -1,9 +1,41 @@
-class SomeKindOfSort {
+class SortOfQuick {
   constructor(array) {
     this.array = array;
   }
+
   sort() {
-    console.log("sort!");
+    const recursionOfSort = (start = 0, end = this.array.length - 1) => {
+      let left = start;
+      let right = end;
+      let pivot = this.array[start];
+
+      while (right >= left) {
+        while (this.array[left] < pivot) {
+          left++;
+        }
+        while (pivot < this.array[right]) {
+          right--;
+        }
+        if (right <= left) {
+          break;
+        }
+
+        let temp = this.array[left];
+        this.array[left] = this.array[right];
+        this.array[right] = temp;
+        left++;
+        right--;
+      }
+
+      if (start < left - 1) {
+        recursionOfSort(start, left - 1);
+      }
+      if (right + 1 < end) {
+        recursionOfSort(right + 1, end);
+      }
+    };
+    recursionOfSort();
+    return this.array;
   }
 
   returnValue(value) {
@@ -11,4 +43,4 @@ class SomeKindOfSort {
   }
 }
 
-module.exports = SomeKindOfSort;
+module.exports = SortOfQuick;
--- test/_sort.js
+++ test/_sort.js
@@ -1,11 +1,25 @@
-const Sort = require("../src/Sort");
+const SortOfQuick = require("../src/Sort");
 const { expect } = require("chai");
 
+let quick;
 describe("Sort", () => {
+  beforeEach(() => {
+    quick = new SortOfQuick([11, 10, 21, 11, 35, 23, -1]);
+  });
+
   it("should be a function", () => {
-    expect(Sort).to.be.a("function");
+    expect(SortOfQuick).to.be.a("function");
   });
   it("should have a sort method", () => {
-    expect(Sort.prototype.sort).to.be.a("function");
+    expect(SortOfQuick.prototype.sort).to.be.a("function");
+  });
+  it("should sort numbers using sort method", () => {
+    expect(quick.sort()).to.deep.equal([-1, 10, 11, 11, 21, 23, 35]);
+  });
+  it("should be a function", () => {
+    expect(SortOfQuick.prototype.returnValue).to.be.a("function");
+  });
+  it("should return value", () => {
+    expect(quick.returnValue("value")).to.equal("value");
   });
 });
